5865512d2c4bb0fc10a55729325d7adacc955fa0
randy <randy@test-cube.com>
2021-04-05 20:32:53 +0700

    add Selenium custom

--- src/test/java/com/example/petland_test/MainPageTest.java
+++ src/test/java/com/example/petland_test/MainPageTest.java
@@ -1,75 +1,33 @@
 package com.example.petland_test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
-import static org.testng.Assert.*;
+import static com.google.common.truth.Truth.*;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-
-import java.util.concurrent.TimeUnit;
 
 public class MainPageTest {
-    private WebDriver driver;
-    private MainPage mainPage;
+    private Selenium s;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless"); // Bypass OS security model
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.jetbrains.com/");
-
-        mainPage = new MainPage(driver);
+    public void setUp() throws InterruptedException {
+        s = new Selenium("https://tiki.vn/");
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        s.quit();
     }
 
     @Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        WebElement searchField = driver.findElement(By.id("header-search"));
-        searchField.sendKeys("Selenium");
-
-        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit' and text()='Search']"));
-        submitButton.click();
-
-        WebElement searchPageField = driver.findElement(By.className("js-search-input"));
-        assertEquals(searchPageField.getAttribute("value"), "Selenium");
-    }
-
-    @Test
-    public void toolsMenu() {
-        new Actions(driver)
-                .moveToElement(mainPage.toolsMenu)
-                .perform();
-
-        WebElement menuPopup = driver.findElement(By.className("menu-main__popup-wrapper"));
-        assertTrue(menuPopup.isDisplayed());
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeAllToolsButton.click();
-
-        WebElement productsList = driver.findElement(By.className("products-list"));
-        assertTrue(productsList.isDisplayed());
-        assertEquals(driver.getTitle(), "All Developer Tools and Products by JetBrains");
+    public void testTitle() {
+        String currentTitle = s.d.getTitle();
+        assertThat(currentTitle).isEqualTo("Tiki - Mua hàng online giá tốt, hàng chuẩn, ship nhanh");
     }
 }
---
+++ src/test/java/com/example/petland_test/Selenium.java
@@ -0,0 +1,88 @@
+package com.example.petland_test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Selenium {
+    WebDriver d;
+
+    public Selenium(){
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--headless"); // Bypass OS security model
+        d = new FirefoxDriver();
+    }
+
+    public Selenium(String url) throws InterruptedException {
+        System.setProperty("webdriver.firefox.driver", "../driver/geckodriver");
+        d = new FirefoxDriver();
+        d.get(url);
+        Thread.sleep(1000);
+    }
+
+    public void input(String css, CharSequence... content){
+        this.byCss(css).sendKeys(content);
+    }
+
+    public void click(String css){
+        this.byCss(css).click();
+    }
+
+    public void hover(String css){
+        WebElement element = byCss(css);
+        Actions action = new Actions(d);
+        action.moveToElement(element).perform();
+    }
+
+    public void select(String css,String value){
+        Select selectList = new Select(this.byCss(css));
+        selectList.selectByValue(value);
+    }
+
+    public void scrollDown(int pixel) throws InterruptedException {
+        ((JavascriptExecutor) d).executeScript("window.scrollBy(0,"+pixel+")", "");
+        Thread.sleep(500);
+    }
+
+    public String getUrl(){
+        return d.getCurrentUrl();
+    }
+
+    public String getText(String css){
+        return byCss(css).getText();
+    }
+
+    public void quit(){
+        d.quit();
+    }
+
+    public WebElement byCss(String css){
+        return d.findElement(By.cssSelector(css));
+    }
+
+    public List<WebElement> listByCss(String css){
+        return d.findElements(By.cssSelector(css));
+    }
+
+    public static String randomStr(int len){
+        return UUID.randomUUID().toString().substring(0,len).replace('-','0');
+    }
+}
