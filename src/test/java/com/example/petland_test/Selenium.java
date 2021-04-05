14b08475d3f63489d77d7053661a04b25c3e2d63
randy <randy@test-cube.com>
2021-04-05 20:36:40 +0700

    setup chrome driver

--- src/test/java/com/example/petland_test/Selenium.java
+++ src/test/java/com/example/petland_test/Selenium.java
@@ -19,7 +19,7 @@ public class Selenium {
     WebDriver d;
 
     public Selenium(){
-        WebDriverManager.firefoxdriver().setup();
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
