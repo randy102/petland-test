2b2c4488029322f06f169bc2a8cdad49fed07098
randy <randy@test-cube.com>
2021-04-07 08:47:36 +0700

    add headless chrome

--- src/test/java/utils/Relenium.java
+++ src/test/java/utils/Relenium.java
@@ -26,7 +26,7 @@ public class Relenium {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        // options.addArguments("--headless");
+        options.addArguments("--headless");
         d = new ChromeDriver(options);
     }
 
