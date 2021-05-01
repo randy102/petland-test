13f61bfd1a4ae2c9527c9a59facaaf6e71990e39
randy <randy@test-cube.com>
2021-05-01 19:43:08 +0700

    test

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -3,10 +3,13 @@ package test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.BasePage;
+import utils.TestListener;
 
 import static com.codeborne.selenide.Selenide.closeWindow;
 
+@Listeners({TestListener.class})
 public abstract class BaseTest<Page extends BasePage> {
     public Page page;
 
